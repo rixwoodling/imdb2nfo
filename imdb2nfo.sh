2f5b06b58f59f2524a3ab32ca5fed75b00905a81
Rix <rixwoodling@gmail.com>
2021-08-11 19:03:01 -0700

    Update imdb2nfo.sh

--- imdb2nfo.sh
+++ imdb2nfo.sh
@@ -20,7 +20,7 @@
 #genre1() { <<< "$imdb" grep -A 2 \"genre\"\: | awk 'FNR == 2 {print}' | sed -e 's/^[ \t]*//' -e 's/"//' | sed -e 's/".*//' ; }
 #genre2() { <<< "$imdb" grep -A 2 \"genre\"\: | awk 'FNR == 3 {print}' | sed -e 's/^[ \t]*//' -e 's/"//' | sed -e 's/".*//' ; }
 # parentalguide
-mpaa() { <<< "$imdbpg" grep certificates\=US | awk -F: '{print $2}' | sed 's/\".*//' ; }
+mpaa() { <<< "$imdbpg" grep certificates\=US | awk -F: '{print $2}' | sed 's/\".*//' | grep G\|PG\|PG-13\|R ; }
 # releaseinfo
 year() { <<< "$imdbri" grep og\:title | awk -F\" '{print $2}' | sed 's/.*\ (//' | sed 's/).*//' ; }
 title() { <<< "$imdbri" grep og\:title | awk -F\" '{print $2}' | sed 's/).*//' | sed 's/\ (.*//' ; }
